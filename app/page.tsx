2e6209260d95714241f045d56d3e81051c07fe15
tajemouti <tajemouti@gmail.com>
2024-08-12 17:53:43 +0100

    Update Home component layout with Tailwind CSS for better responsiveness and centering

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>Hello Nextjs Porfolio</h1>
       </div>
     </main>

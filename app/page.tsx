4e90b3bf42927562fbfd9ca1ea96b1fa6521396f
tajemouti <tajemouti@gmail.com>
2024-08-14 02:08:15 +0100

    Add Grid component to display BentoGrid items

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
@@ -13,6 +14,7 @@ export default function Home() {
           ]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

b73df5d895a870b385d420fbbe9626cb239a5a31
tajemouti <tajemouti@gmail.com>
2024-08-25 23:43:45 +0100

    Update the project component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,8 +13,8 @@ const Projects = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
             <PinContainer
-              title={title}
-              href=""
+              title={link}
+              href={link}
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
